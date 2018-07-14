5f9d0ca0c20196cf5a31964977860458f3703754
Tracy Fussell <tracy.fussell@gmail.com>
2018-07-14 13:27:50 -0400

    went off book since no bugs in mine

--- js/app.js
+++ js/app.js
@@ -1,20 +1,70 @@
 /*
  * Create a list that holds all of your cards
  */
+
+let toggledCards = [];
 const deck = document.querySelector('.deck'); //create variable deck, define as element with class deck
 console.log('deck'); //print word deck, type word deck will show children elements of deck
 
-deck.addEventListener('click', event => { //add event listener to deck instead of individual cards to improve performance
+deck.addEventListener('click', event => { //add event listener to deck instead of individual cards to improve performance.
+  //Clicking on the element degines it as the target
   const clickTarget = event.target; //create variable clickTarget and assign target event to it
-  if(clickTarget.classList.contains('card')){ //parameter search for any elements in deck with a class of card.
-                                             //  Since the event listener is added to the deck, we know thatt the target is the deeck.
-  clickTarget.classList.toggle('open') ; //clicking on the target will add the class 'open' because it isn't there.
-                                        //  If I click on the card again it will remove the class 'open'
-  clickTarget.classList.toggle('show') ; //clicking on the target will add the class 'show' because it isn't there.
-                                        //  If I click on the card again it will remove the class 'show'
-}
-  });
+  if(clickTarget.classList.contains('card')&& //parameter search for any elements in deck with a class of card
+  toggledCards.length < 2){ //and a second parameter that there are less than 2 cards in the toggledCards array then
+    //toggleCards and addToggleCard functions can fire
+   toggleCard(clickTarget); //calls the function toggleCard and applies it to the parameter clickTarget
+   addToggleCard(clickTarget);//calls function to add toggled cards to array.
 
+   if (toggledCards.length === 2){ //if statement to check if the length of the toggledCards array is equal to 2 cards.
+     console.log("That's two cards"); // print message if the toggledCards array is equal to 2
+     checkForMatch();//Call function that checks for matched cards
+   }
+}
+});
+
+function toggleCard(clickTarget) { //By turning this in to a function, the clicl event/target is part of the function parameter
+  clickTarget.classList.toggle('open') ; //adds the class 'open' because it isn't there.
+                            //click on the card again it will remove the class 'open'
+  clickTarget.classList.toggle('show') ; //add the class 'show' because it isn't there.
+                            //click on the card again it will remove the class 'show'
+}
+
+
+function addToggleCard(clickTarget){ //create a function to add the toggled cards
+                                    //(identified by they fact that they object of the clickTarget) to an array
+  toggledCards.push(clickTarget); //any card that is clicked on will be added to the toggledCards array.
+                                  //it is possible to add event listeners to arrays but not to nodeLists
+  console.log(toggledCards);  //print toggeled cards aray to console.
+}
+
+function checkForMatch(){ //function to checking if the cards in the toggledCards array are a match
+  if ( //if statement comparing the className or the firstElementChild of the first position of the the toggledCards arrays
+       //with the classname of the firstElementChild of the second position of the toggledCards array
+    toggledCards[0].firstElementChild.className === //using the console, you can see that the firstElementChild contains the
+                                                    //class name we are trying to compare for each element in the array
+    toggledCards[1].firstElementChild.className
+  )
+  {
+    toggledCards[0].classList.toggle('match'); // if there is a match between the first and second items in the array, we toggle
+                                              //the first card's class to 'match', since there isn't already an existing 'match' class,
+                                              //its added to the element
+    toggledCards[1].classList.toggle('match')// if there is a match between the first and second items in the array, we toggle
+                                              //the second card's class to 'match', since there isn't already an existing 'match' class,
+                                              //its added to the element
+    toggledCards =[];
+  } else {
+    console.log('Not a match!');
+       setTimeout(() => {
+           toggleCard(toggledCards[0]);// if there is no match between the first and second items in the array, we call the function
+                                 //toggleCard with a parameter of first item in the toggledCards array. This will toggle the first
+                                 //card's classses of 'open' amd 'show' to removed
+           toggleCard(toggledCards[1]);// if there is no match between the first and second items in the array, we call the function
+                                 //toggleCard with a parameter of second item in the toggledCards array. This will toggle the second
+                                 //card's classses of 'open' amd 'show' to removed
+           toggledCards = []; //clears the toggledCard array so it can be used again for the next two card selections
+  }, 1000); // pauses the execution of code for 1000ms so that the unmatched second card can be seen before the array is reset.
+  }
+}
 
 
 /*
